7e21aca0afb39d41ea2127cd873b1769b70b58ae
ken pepple <ken@pepple.io>
2026-05-18 10:33:06 -0700

    feat(rsu): add widget definition for service proxy

---
+++ src/widgets/rsu/widget.js
@@ -0,0 +1,15 @@
+import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
+
+const widget = {
+  api: `https://finnhub.io/api/{endpoint}`,
+  proxyHandler: credentialedProxyHandler,
+  mappings: {
+    quote: {
+      // https://finnhub.io/docs/api/quote
+      endpoint: "v1/quote",
+      params: ["symbol"],
+    },
+  },
+};
+
+export default widget;
